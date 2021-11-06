a251c9d2040aa5b4a9b3aebd800069e3bc5adb04
JasmineCAicai <1105111422@qq.com>
2021-11-06 22:11:12 +0800

    Add bottom navigation bar
    Add three fragments, home, FAQ and profile.

--- app/src/main/java/com/example/uncovid/MainActivity.kt
+++ app/src/main/java/com/example/uncovid/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.uncovid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.uncovid.fragments.AsksFragment
+import com.example.uncovid.fragments.HomeFragment
+import com.example.uncovid.fragments.ProfileFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        val homeFragment = HomeFragment()
+        val asksFragment = AsksFragment()
+        val profileFragment = ProfileFragment()
+
+        makeCurrentFragment(homeFragment)
+
+        navView.setOnNavigationItemSelectedListener {
+            when (it.itemId){
+                R.id.ic_home -> makeCurrentFragment(homeFragment)
+                R.id.ic_question_answer -> makeCurrentFragment(asksFragment)
+                R.id.ic_profile -> makeCurrentFragment(profileFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
 }
