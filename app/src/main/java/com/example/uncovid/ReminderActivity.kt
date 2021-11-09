bd9b02afb67f9e7964119e239b387c0efc7bb9f0
JasmineCAicai <1105111422@qq.com>
2021-11-09 12:03:00 +0800

    Add Reminder Detail Activity

--- app/src/main/java/com/example/uncovid/ReminderActivity.kt
+++ app/src/main/java/com/example/uncovid/ReminderActivity.kt
@@ -1,5 +1,6 @@
 package com.example.uncovid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_reminder.*
@@ -12,5 +13,21 @@ class ReminderActivity : AppCompatActivity() {
         remBack1Btn.setOnClickListener {
             finish()
         }
+
+        knowCorBtn.setOnClickListener {
+            viewReminderDetail(1)
+        }
+        knowAvoidBtn.setOnClickListener {
+            viewReminderDetail(2)
+        }
+        knowTreatBtn.setOnClickListener {
+            viewReminderDetail(3)
+        }
+    }
+
+    private fun viewReminderDetail(option: Int) {
+        val intent = Intent(this, ReminderDetailActivity::class.java)
+        intent.putExtra("option", option)
+        startActivity(intent)
     }
 }
