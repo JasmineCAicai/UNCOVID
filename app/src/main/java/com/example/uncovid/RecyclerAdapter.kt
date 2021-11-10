527e8ccd03744646f9c7d82a4600b167dae78d6b
JasmineCAicai <1105111422@qq.com>
2021-11-10 14:27:49 +0800

    Create RecyclerView 2
    Add FAQ question list

--- app/src/main/java/com/example/uncovid/RecyclerAdapter.kt
+++ app/src/main/java/com/example/uncovid/RecyclerAdapter.kt
@@ -5,16 +5,22 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.question_card_layout.view.*
 
 class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
 
-    private var titles = arrayOf("Chapter One", "Chapter Two", "Chapter Three", "Chapter Four", "Chapter Five", "Chapter Six", "Chapter Seven", "Chapter Eight")
+    private var titles = arrayOf("What is COVID-19?", "What is COVID-19?",
+        "What is COVID-19?", "What is COVID-19?",
+        "What is COVID-19?", "What is COVID-19?",
+        "What is COVID-19?", "What is COVID-19?")
 
-    private var dates = arrayOf("Jan 1, 2021", "Jan 2, 2021", "Jan 3, 2021", "Jan 4, 2021", "Jan 5, 2021", "Jan 6, 2021", "Jan 7, 2021", "Jan 8, 2021")
+    private var dates = arrayOf("Jan 1, 2021", "Jan 2, 2021", "Jan 3, 2021", "Jan 4, 2021",
+        "Jan 5, 2021", "Jan 6, 2021", "Jan 7, 2021", "Jan 8, 2021")
 
-    private var images = intArrayOf(R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle)
+    private var images = intArrayOf(R.mipmap.circle, R.mipmap.circle, R.mipmap.circle,
+        R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle, R.mipmap.circle)
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerAdapter.ViewHolder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.question_card_layout, parent, false)
@@ -36,5 +42,12 @@ class RecyclerAdapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
         var itemTitle: TextView = itemView.item_title
         var itemDate: TextView = itemView.item_date
 
+        init {
+            itemView.setOnClickListener {
+                val position: Int = adapterPosition
+
+                Toast.makeText(itemView.context, "you clicked on ${titles[position]}", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
