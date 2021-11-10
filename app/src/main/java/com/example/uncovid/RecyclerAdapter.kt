b39377c96bb999f1f31071d7cf869a5e6d09321f
JasmineCAicai <1105111422@qq.com>
2021-11-10 16:40:27 +0800

    Create Add Answer 1

--- app/src/main/java/com/example/uncovid/RecyclerAdapter.kt
+++ app/src/main/java/com/example/uncovid/RecyclerAdapter.kt
@@ -1,7 +1,5 @@
 package com.example.uncovid
 
-import android.app.Activity
-import android.content.Context
 import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +7,6 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
-import androidx.core.content.ContextCompat.startActivity
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.question_card_layout.view.*
 
