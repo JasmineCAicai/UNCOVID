e0afc52636d0b89932d099e134efed373368ef7a
WX <1447870798@qq.com>
2021-11-25 02:59:34 +0800

    Location List UI updated

---
+++ app/src/main/java/com/example/uncovid/LocationListAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.uncovid
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class LocationListAdapter(private val context: Context,
+                          private val location: Array<String>?,
+                          private val time: Array<String>?): RecyclerView.Adapter<LocationListAdapter.ViewHolder>() {
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        var itemLocation: TextView = itemView.findViewById(R.id.tvLocation)
+        var itemTime :TextView = itemView.findViewById(R.id.tvTime)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val v = LayoutInflater.from(context).inflate(R.layout.custom_location_card,parent, false)
+        return ViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        if(location != null)
+            holder.itemLocation.text = location!![position]
+        if(time != null)
+            holder.itemTime.text = time!![position]
+    }
+
+    override fun getItemCount(): Int {
+        if (location != null) {
+            return location.size
+        }
+        return 0
+    }
+}
