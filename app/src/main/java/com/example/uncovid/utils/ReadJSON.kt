73736859df71337c0a5acd3de49e33f0accc34ec
JasmineCAicai <1105111422@qq.com>
2021-12-24 22:58:46 +0800

    Implement Q&A
    Create JSON file to contain information of questions.

---
+++ app/src/main/java/com/example/uncovid/utils/ReadJSON.kt
@@ -0,0 +1,23 @@
+package com.example.uncovid.utils
+import android.content.Context
+import java.io.IOException
+
+/*
+class ReadJSON {
+
+}
+
+ */
+
+fun getJSONDataFromAsset(context: Context, fileName: String): String? {
+    val jsonString: String
+    try {
+        jsonString = context.assets.open(fileName).bufferedReader().use {
+            it.readText()
+        }
+    } catch (ioException: IOException) {
+        ioException.printStackTrace()
+        return null
+    }
+    return jsonString
+}
