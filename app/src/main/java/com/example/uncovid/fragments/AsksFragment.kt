d0d72a3b96fe40470832781030490a7183533fa1
JasmineCAicai <1105111422@qq.com>
2021-12-28 13:28:38 +0800

    Fix bug
    Fix bug on question list.

--- app/src/main/java/com/example/uncovid/fragments/AsksFragment.kt
+++ app/src/main/java/com/example/uncovid/fragments/AsksFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
 import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.uncovid.R
 import com.example.uncovid.RecyclerAdapter
 import com.example.uncovid.SharedViewModel
+import com.example.uncovid.entity.Answer
 import kotlinx.android.synthetic.main.fragment_asks.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -24,7 +26,8 @@ class AsksFragment : Fragment() {
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
     private var layoutManager: RecyclerView.LayoutManager? = null
-    private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    //private var adapter: RecyclerView.Adapter<RecyclerAdapter.ViewHolder>? = null
+    lateinit var adapter: RecyclerAdapter
 
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -49,11 +52,37 @@ class AsksFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        searchBox.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+
+        })
+
+        getQuestionList()
+    }
+
+    private fun getQuestionList() {
+        var questionList = ArrayList<Answer>()
+        questionList.add(Answer(0, "What is COVID-19?", "Jan 1, 2021", ""))
+        questionList.add(Answer(1, "What causes COVID-19?", "Jan 2, 2021", ""))
+        questionList.add(Answer(2, "Where was COVID-19 first discovered?", "Jan 3, 2021", ""))
+        questionList.add(Answer(3, "How does COVID-19 spread?", "Jan 4, 2021", ""))
+        questionList.add(Answer(4, "What are coronaviruses?", "Jan 5, 2021", ""))
+        questionList.add(Answer(5, "How do viruses get their name?", "Jan 6, 2021", ""))
+        questionList.add(Answer(6, "In what conditions does COVID-19 survive?", "Jan 7, 2021", ""))
+        questionList.add(Answer(7, "When COVID-19 will spread more easily?", "Jan 8, 2021", ""))
+
         layoutManager = LinearLayoutManager(activity)
 
         recyclerView.layoutManager = layoutManager
 
-        adapter = RecyclerAdapter()
+        adapter = RecyclerAdapter(questionList)
         recyclerView.adapter = adapter
     }
 
